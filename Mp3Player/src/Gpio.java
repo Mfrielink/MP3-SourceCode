a431eb0456eff9500f3eb67e181c756e109d8dfb
Gertjuh92 <gertjuh92@live.nl>
2014-05-16 15:07:10 +0200

    versie 2.0
    in eclipse gezet Gpio aangepast

--- Mp3Player/src/Gpio.java
+++ Mp3Player/src/Gpio.java
@@ -1,7 +1,9 @@
-public class Gpio {
-	// Native methods declaration
+class Gpio {
+	/* Native methods declaration */
 	public native int ioinit();
+
 	public native int iodeinit();
+
 	public native int iowrite(int a, int v);
 
 	public native int ioread(int a);
@@ -12,20 +14,5 @@ public class Gpio {
 	}
 
 	// Main function calls native method
-	public static void main(String[] args) {
-		Gpio io = new Gpio();
-
-		io.ioinit(); // Initialize GPIO lines
-		for (int i = 0; i < 200000; i++) {
-			io.iowrite(80, 1); // Make output PB16 high
-			System.out.println("PB30: " + io.ioread(94)); // Read status of
-															// input PB30
-			io.iowrite(80, 0); // Make output PB16 low
-			System.out.println("PB30: " + io.ioread(94)); // Read status of
-															// input PB30
-		}
-
-		io.iodeinit(); // Deinitialize GPIO lines
-	}// main
 
 }
