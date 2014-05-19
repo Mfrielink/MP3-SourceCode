102bf7d481a6f75c8e35645825e8f46216243beb
Martijn <mfrielin@student.avans.nl>
2014-05-19 18:46:57 +0200

    inhoud InfoSd toegevoegd

--- Mp3Player/InfoSd.java
+++ Mp3Player/InfoSd.java
@@ -1,6 +1,70 @@
+import java.io.File;
+import java.io.IOException;
 
 
-
+@SuppressWarnings("unused")
 public class InfoSd {
 
+	private String title;
+	private long time;
+
+	File folder = new File("C:/Users/Martijn/Documents/Files");
+	File[] listOfFiles = folder.listFiles();
+
+	public String getSongName(int x) {
+/*
+		Mp3File mp3file = null;
+
+		try {
+			try {
+				mp3file = new Mp3File("C:/Users/Martijn/Documents/Files/"
+						+ listOfFiles[x].getName());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (UnsupportedTagException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidDataException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		String artist = mp3file.getId3v2Tag().getArtist();
+		String name = mp3file.getId3v2Tag().getTitle();
+		title = artist + " - " + name;
+		System.out.println(title);
+*/
+		return title;
+		
+	}
+
+	public long getSongTime(int x) {
+/*
+		Mp3File mp3file = null;
+		try {
+			mp3file = new Mp3File("C:/Users/Martijn/Documents/Files/"
+					+ listOfFiles[x].getName());
+		} catch (UnsupportedTagException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidDataException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println("Length of this mp3 is: "
+				+ mp3file.getLengthInSeconds() + " seconds");
+
+		time = mp3file.getLengthInSeconds();
+
+		// Add function for changing total seconds to minutes + seconds.
+*/
+		return time;
+	}
+	
 }
